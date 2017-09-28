b7aa7a95de3c7285cc4f30978b396fdd04f22a46
Egor <developerEgor@yandex.ru>
2017-09-28 15:58:54 +0300

    version #3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,11 +9,28 @@ public class Main {
 
         String[] array  = {str0, str1, str2};
         print(array);
+        sort(array);
     }
 
     public static void print(String[] mas){
         for (int i=0; i < mas.length; i++)
         System.out.println(mas[i] + " ");
         System.out.println("\n");
+    }
+    public static void sort(String[] mas){
+        String sortedA;
+        for (int i=0; i < mas.length; i++){
+            for (int j = 1; j < mas.length; j++){
+                if (mas[i].compareTo(mas[j]) < 0){
+                    sortedA = mas[j];
+                    mas[j] = mas[i];
+                    mas[i] = sortedA;
+                }
+            }
+        }
+
+        for (int i = 0; i < mas.length; i++){
+            System.out.println(mas[i]);
+        }
     }
 }
