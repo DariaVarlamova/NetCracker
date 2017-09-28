c32892bebe1bab8789bb71781a2a42dfedd51a76
Egor <developerEgor@yandex.ru>
2017-09-28 15:52:11 +0300

    version #2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String str0 = "This is a first row";
+        String str1 = "This is a second row";
+        String str2 = "This is a third row";
+
+        String[] array  = {str0, str1, str2};
+        print(array);
+    }
+
+    public static void print(String[] mas){
+        for (int i=0; i < mas.length; i++)
+        System.out.println(mas[i] + " ");
+        System.out.println("\n");
     }
 }
